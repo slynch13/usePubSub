048c0a9e95051a6a098bfba6d0f6c2f7beb9a4b7
Sean Lynch <slynch@gmail.com>
2019-08-28 20:10:47 -0400

    Updated to fix linting rules

--- src/usePubSub.js
+++ src/usePubSub.js
@@ -1,75 +1,76 @@
-import React, { useContext, useReducer, useEffect, useRef } from "react";
+import React, { useReducer, useEffect, useRef } from 'react'
 
-//Provide an app wide pub sub context
-const DefaultPubSubContext = React.createContext();
+// Provide an app wide pub sub context
+const DefaultPubSubContext = React.createContext()
 const usePubSub = context => {
-  let PubSubContext = DefaultPubSubContext;
+  let PubSubContext = DefaultPubSubContext
 
-  if (context !== undefined) PubSubContext = context;
+  if (context !== undefined) PubSubContext = context
+  // eslint-disable-next-line no-unused-vars
   const [subscriptionManager, dispatch] = useReducer(
     (state, action) => {
-      let current = { ...state };
+      let current = { ...state }
       switch (action.type) {
-        case "Subscribe":
-          current.channels = { ...state.channels };
+        case 'Subscribe':
+          current.channels = { ...state.channels }
+          let handlers = current.channels[action.channel] || []
+          handlers = [...handlers]
 
-          let handlers = current.channels[action.channel] || [];
-          handlers = [...handlers];
+          handlers.push(action.handler)
+          current.channels[action.channel] = handlers
+          break
+        case 'Unsubscribe':
+          let unsubHandlers = current.channels[action.channel] || []
+          unsubHandlers = [...unsubHandlers]
 
-          handlers.push(action.handler);
-          current.channels[action.channel] = handlers;
-          break;
-        case "Unsubscribe":
-          //current.channels = { ...state.channels };
-          let unsubHandlers = current.channels[action.channel] || [];
-          unsubHandlers = [...unsubHandlers];
+          let i = unsubHandlers.indexOf(action.handler)
+          if (i > -1) unsubHandlers.splice(i, 1)
 
-          let i = unsubHandlers.indexOf(action.handler);
-          if (i > -1) unsubHandlers.splice(i, 1);
+          current.channels[action.channel] = unsubHandlers
 
-          current.channels[action.channel] = unsubHandlers;
-
-          break;
-        case "Publish":
-          let channel = current.channels[action.channel] || [];
+          break
+        case 'Publish':
+          let channel = current.channels[action.channel] || []
           for (let i = 0; i < channel.length; i++) {
-            channel[i](action.message);
+            channel[i](action.message)
           }
+          break
+
         default:
-          //Return exiting state
-          return state;
+          // Return exiting state
+          return state
       }
-      return current;
+      return current
     },
     { channels: {} }
-  );
-  let ref = useRef({ dispatch });
+  )
+  let ref = useRef({ dispatch })
   useEffect(
     () => {
-      ref.current = { dispatch };
+      ref.current = { dispatch }
     },
     [dispatch]
-  );
+  )
 
   let subscribe = (channel, handler) => {
-    if (!ref.current) return () => {};
-    ref.current.dispatch({ type: "Subscribe", channel, handler });
-    return () => unsubscribe(channel, handler);
-  };
+    if (!ref.current) return () => { }
+    ref.current.dispatch({ type: 'Subscribe', channel, handler })
+    return () => unsubscribe(channel, handler)
+  }
   let unsubscribe = (channel, handler) => {
-    if (!ref.current) return () => {};
-    ref.current.dispatch({ type: "Unsubscribe", channel, handler });
-  };
+    if (!ref.current) return () => { }
+    ref.current.dispatch({ type: 'Unsubscribe', channel, handler })
+  }
   let publish = (channel, message) => {
-    if (!ref.current) return () => {};
-    ref.current.dispatch({ type: "Publish", channel, message });
-  };
+    if (!ref.current) return () => { }
+    ref.current.dispatch({ type: 'Publish', channel, message })
+  }
   return {
     subscribe: subscribe,
     unsubscribe: unsubscribe,
     publish: publish,
     PubSubContext
-  };
-};
-export default usePubSub;
-export { DefaultPubSubContext };
+  }
+}
+export default usePubSub
+export { DefaultPubSubContext }
